1de413bbb84625676b1a2ee7d70eb6a976a8dca4
superbeom <it.super.beom@gmail.com>
2022-12-14 13:12:33 +0900

    Update Add > Add Profile and Auth Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Auth from "./pages/auth/Auth";
 
 const App = () => {
   return (
@@ -7,6 +9,8 @@ const App = () => {
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
       <Home />
+      <Profile />
+      <Auth />
     </div>
   );
 };
