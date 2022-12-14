0b06fade88db7c28419cb18f57ec51d136e553b3
superbeom <it.super.beom@gmail.com>
2022-12-14 12:01:39 +0900

    Update PostShare

--- src/components/postShare/PostShare.jsx
+++ src/components/postShare/PostShare.jsx
@@ -1,13 +1,29 @@
-import React from "react";
-import { UilScenery } from "@iconscout/react-unicons";
-import { UilPlayCircle } from "@iconscout/react-unicons";
-import { UilLocationPoint } from "@iconscout/react-unicons";
-import { UilSchedule } from "@iconscout/react-unicons";
+import React, { useState, useRef } from "react";
+import {
+  UilScenery,
+  UilPlayCircle,
+  UilLocationPoint,
+  UilSchedule,
+  UilTimes,
+} from "@iconscout/react-unicons";
 
 import "./PostShare.css";
 import ProfileImage from "../../img/profileImg.jpg";
 
 const PostShare = () => {
+  const [image, setImage] = useState(null);
+  const imageRef = useRef();
+
+  const onImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      let img = e.target.files[0];
+
+      setImage(URL.createObjectURL(img));
+
+      URL.revokeObjectURL(img);
+    }
+  };
+
   return (
     <div className="PostShare">
       <img src={ProfileImage} alt="profile" />
@@ -16,7 +32,11 @@ const PostShare = () => {
         <input type="text" placeholder="What's happening" />
 
         <div className="postOptions">
-          <div className="option" style={{ color: "var(--photo)" }}>
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={() => imageRef.current.click()}
+          >
             <UilScenery />
             Photo
           </div>
@@ -37,7 +57,23 @@ const PostShare = () => {
           </div>
 
           <button className="button ps-button">Share</button>
+
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
+
+        {image && (
+          <div className="previewImage">
+            <UilTimes onClick={() => setImage(null)} />
+            <img src={image} alt="upload" />
+          </div>
+        )}
       </div>
     </div>
   );
